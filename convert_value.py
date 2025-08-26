f2e59dd9cf740837f5aadae2a3f6b0f768534092
riw11 <ariqrafirs@gmail.com>
2025-08-26 22:53:14 +0700

    making a simple convert program

---
+++ convert_value.py
@@ -0,0 +1,14 @@
+#input value
+num = int(input("Enter a value: "))
+
+#convert into integer
+print(f"The sum is {num:d}")
+
+#convert into float
+print(f"The sum is {num:f}")
+
+#convert into hexadecimal
+print(f"The sum is {num:x}")
+
+#convert into octal
+print(f"The sum is {num:o}")
